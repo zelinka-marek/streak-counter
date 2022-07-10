bbdfeb7caba2f5aa98bd0ac516d80b09974ab930
Marek Zelinka <mzelinka17@gmail.com>
2022-07-10 21:00:33 +0200

    increment streak

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,46 @@ interface Streak {
 
 const KEY = "streak";
 
+function shouldIncrementOrResetStreakCount(
+  currentDate: Date,
+  lastLoginDate: string
+): "increment" | undefined {
+  // We get 11/5/2021
+  // so to get 5, we split on / and get the second item
+  const difference =
+    currentDate.getDate() - parseInt(lastLoginDate.split("/")[0]);
+  // This means they logged in the day after the currentDate
+  if (difference === 1) {
+    return "increment";
+  }
+  // Otherwise they logged in after a day, which would
+  // break the streak
+  return undefined;
+}
+
 export function streakCounter(storage: Storage, date: Date): Streak {
   const streakInLocalStorage = storage.getItem(KEY);
   if (streakInLocalStorage) {
     try {
-      const streak = JSON.parse(streakInLocalStorage || "");
+      const streak = JSON.parse(streakInLocalStorage);
+      const state = shouldIncrementOrResetStreakCount(
+        date,
+        streak.lastLoginDate
+      );
+      const SHOULD_INCREMENT = state === "increment";
+
+      if (SHOULD_INCREMENT) {
+        const updatedStreak = {
+          ...streak,
+          currentCount: streak.currentCount + 1,
+          lastLoginDate: formattedDate(date),
+        };
+        // store in localStorage
+        storage.setItem(KEY, JSON.stringify(updatedStreak));
+
+        return updatedStreak;
+      }
+
       return streak;
     } catch (error) {
       console.error("Failed to parse streak from localStorage");
