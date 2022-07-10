054a4be95ce1d3ddd29c102e02ac0e218a61beb4
Marek Zelinka <mzelinka17@gmail.com>
2022-07-10 19:07:46 +0200

    add getStreak function

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -43,4 +43,37 @@ describe("streakCounter", () => {
     const streakAsString = mockLocalStorage.getItem(key);
     expect(streakAsString).not.toBeNull();
   });
+
+  describe("with a pre-populated streak", () => {
+    let mockLocalStorage: Storage;
+
+    beforeEach(() => {
+      const mockJSDom = new JSDOM("", { url: "https://localhost" });
+
+      mockLocalStorage = mockJSDom.window.localStorage;
+
+      // Use date in past so it's always the same
+      const date = new Date("12/12/2021");
+
+      const streak = {
+        currentCount: 1,
+        startDate: formattedDate(date),
+        lastLoginDate: formattedDate(date),
+      };
+
+      mockLocalStorage.setItem("streak", JSON.stringify(streak));
+    });
+
+    afterEach(() => {
+      mockLocalStorage.clear();
+    });
+
+    it("should return the streak from localStorage", () => {
+      const date = new Date();
+      const streak = streakCounter(mockLocalStorage, date);
+
+      // Should match the dates used to set up the tests
+      expect(streak.startDate).toBe("12/12/2021");
+    });
+  });
 });
