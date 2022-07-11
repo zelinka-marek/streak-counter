9d837d0a4d5892ddba6b34cf1f957f7bc8ae5d19
Marek Zelinka <mzelinka17@gmail.com>
2022-07-11 11:12:15 +0200

    reset streak

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -69,10 +69,11 @@ describe("streakCounter", () => {
     });
 
     it("should return the streak from localStorage", () => {
-      const date = new Date();
+      const date = new Date("12/12/2021");
       const streak = streakCounter(mockLocalStorage, date);
 
       // Should match the dates used to set up the tests
+      console.log(streak.startDate);
       expect(streak.startDate).toBe("12/12/2021");
     });
 
@@ -102,10 +103,53 @@ describe("streakCounter", () => {
 
       const streakAsString = mockLocalStorage.getItem(key);
       // Normally you should wrap in try/catch in case the JSON is bad
-      // but sience we authored it, we can skip here
+      // but since we authored it, we can skip here
       const streak = JSON.parse(streakAsString || "");
 
       expect(streak.currentCount).toBe(2);
+    });
+
+    it("should reset if not consecutive", () => {
+      const date = new Date("12/13/2021");
+      const streak = streakCounter(mockLocalStorage, date);
+
+      expect(streak.currentCount).toBe(2);
+
+      // Skip a day and break the streak
+      const dateUpdated = new Date("12/15/2021");
+      const streakUpdated = streakCounter(mockLocalStorage, dateUpdated);
+
+      expect(streakUpdated.currentCount).toBe(1);
+    });
+
+    it("should save the reset streak to localStorage", () => {
+      const key = "streak";
+      const date = new Date("12/13/2021");
+      // Call it once so it updates the streak
+      streakCounter(mockLocalStorage, date);
+
+      // Skip a day and break the streak
+      const dateUpdated = new Date("12/15/2021");
+      const streakUpdated = streakCounter(mockLocalStorage, dateUpdated);
+
+      const streakAsString = mockLocalStorage.getItem(key);
+      // Normally you should wrap in try/catch in case the JSON is bad
+      // but since we authored it, we can skip here
+      const streak = JSON.parse(streakAsString || "");
+
+      expect(streak.currentCount).toBe(1);
+    });
+
+    it("should not reset the streak for same-day login", () => {
+      const date = new Date("12/13/2021");
+      // Call it once so it updates the streak
+      streakCounter(mockLocalStorage, date);
+
+      // Simulate same-day login
+      const dateUpdated = new Date("12/13/2021");
+      const streakUpdated = streakCounter(mockLocalStorage, dateUpdated);
+
+      expect(streakUpdated.currentCount).toBe(2);
     });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -11,18 +11,22 @@ const KEY = "streak";
 function shouldIncrementOrResetStreakCount(
   currentDate: Date,
   lastLoginDate: string
-): "increment" | undefined {
+): "increment" | "reset" | "none" {
   // We get 11/5/2021
   // so to get 5, we split on / and get the second item
   const difference =
-    currentDate.getDate() - parseInt(lastLoginDate.split("/")[0]);
+    currentDate.getDate() - parseInt(lastLoginDate.split("/")[1]);
+  // Same-day login, do nothing
+  if (difference === 0) {
+    return "none";
+  }
   // This means they logged in the day after the currentDate
   if (difference === 1) {
     return "increment";
   }
   // Otherwise they logged in after a day, which would
   // break the streak
-  return undefined;
+  return "reset";
 }
 
 export function streakCounter(storage: Storage, date: Date): Streak {
@@ -35,16 +39,26 @@ export function streakCounter(storage: Storage, date: Date): Streak {
         streak.lastLoginDate
       );
       const SHOULD_INCREMENT = state === "increment";
+      const SHOULD_RESET = state === "reset";
 
       if (SHOULD_INCREMENT) {
-        const updatedStreak = {
+        const updatedStreak: Streak = {
           ...streak,
           currentCount: streak.currentCount + 1,
           lastLoginDate: formattedDate(date),
         };
         // store in localStorage
         storage.setItem(KEY, JSON.stringify(updatedStreak));
-
+        return updatedStreak;
+      }
+      if (SHOULD_RESET) {
+        const updatedStreak: Streak = {
+          currentCount: 1,
+          startDate: formattedDate(date),
+          lastLoginDate: formattedDate(date),
+        };
+        // store in localStorage
+        storage.setItem(KEY, JSON.stringify(updatedStreak));
         return updatedStreak;
       }
 
